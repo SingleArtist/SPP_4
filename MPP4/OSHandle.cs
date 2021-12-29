9d4d767f2fc9f15ec6d18b2e390b66eb3d3260ef
SingleArtist <95147229+SingleArtist@users.noreply.github.com>
2021-12-29 20:22:46 +0300

    Add files via upload

---
+++ MPP4/OSHandle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+//Задание 4
+/*Создать класс на языке C#, который:
+- называется OSHandle и обеспечивает автоматическое или
+принудительное освобождение заданного дескриптора
+операционной системы; -содержит свойство Handle, позволяющее установить и получить
+дескриптор операционной системы;
+-реализует метод Finalize для автоматического освобождения
+дескриптора;
+-реализует интерфейс IDisposable для принудительного
+освобождения дескриптора;*/
+
+namespace FourthTask
+{
+    class OSHandle 
+    {
+        [DllImport("Kernel32")]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+        public IntPtr hObject;
+        private bool _isDisposed;
+
+        public OSHandle(IntPtr hObject)
+        {
+            if (hObject == IntPtr.Zero)
+            {
+                throw new ArgumentException("Сan't be null");
+            }
+
+            this.hObject = hObject;
+        }
+
+        ~OSHandle()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (!_isDisposed)
+            {
+                CloseHandle(hObject);
+                _isDisposed = true;
+            }
+        }
+    }
+}
---
+++ MPP4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using FourthTask;
+using Microsoft.Win32.SafeHandles;
+
+//Задание 4
+/*Создать класс на языке C#, который:
+- называется OSHandle и обеспечивает автоматическое или
+принудительное освобождение заданного дескриптора
+операционной системы; -содержит свойство Handle, позволяющее установить и получить
+дескриптор операционной системы;
+-реализует метод Finalize для автоматического освобождения
+дескриптора;
+-реализует интерфейс IDisposable для принудительного
+освобождения дескриптора;*/
+
+namespace FourthTask
+{
+    class Program
+    {
+        static void Main(string[] args){}
+    }
+}
